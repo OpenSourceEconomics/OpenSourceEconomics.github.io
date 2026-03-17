9621c6f6d40391f3758d9462bce475cae98ac256
Hans-Martin von Gaudecker <hmgaudecker@gmail.com>
2026-03-17 06:25:25 +0100

    pylcm: publish benchmarks for a7880a686c01 (machine: 3c4fb8e3)

--- pylcm-benchmarks/dev/bench/data.js
+++ pylcm-benchmarks/dev/bench/data.js
@@ -413,6 +413,111 @@ window.BENCHMARK_DATA = {
             "extra": "rounds: 6, min: 0.162452, max: 0.178763"
           }
         ]
+      },
+      {
+        "commit": {
+          "id": "a7880a686c01e717e7f52a5a6eb66500a0619977",
+          "message": "",
+          "timestamp": "2026-03-17T06:21:12+01:00",
+          "author": {
+            "name": "",
+            "email": ""
+          }
+        },
+        "date": 1773724872000,
+        "benches": [
+          {
+            "name": "test_grid_lookup[lin-500]",
+            "unit": "s",
+            "value": 0.13749315112545446,
+            "range": "\u00b1 0.015851",
+            "extra": "rounds: 8, min: 0.122898, max: 0.164274"
+          },
+          {
+            "name": "test_grid_lookup[lin-1000]",
+            "unit": "s",
+            "value": 0.12763316228665644,
+            "range": "\u00b1 0.004588",
+            "extra": "rounds: 7, min: 0.123092, max: 0.136270"
+          },
+          {
+            "name": "test_grid_lookup[lin-2000]",
+            "unit": "s",
+            "value": 0.14781201285638548,
+            "range": "\u00b1 0.014751",
+            "extra": "rounds: 7, min: 0.128769, max: 0.164941"
+          },
+          {
+            "name": "test_grid_lookup[irreg-500]",
+            "unit": "s",
+            "value": 0.16129313014243962,
+            "range": "\u00b1 0.003586",
+            "extra": "rounds: 7, min: 0.155967, max: 0.165695"
+          },
+          {
+            "name": "test_grid_lookup[irreg-1000]",
+            "unit": "s",
+            "value": 0.16840827600026387,
+            "range": "\u00b1 0.008165",
+            "extra": "rounds: 6, min: 0.156663, max: 0.177088"
+          },
+          {
+            "name": "test_grid_lookup[irreg-2000]",
+            "unit": "s",
+            "value": 0.23960360099881656,
+            "range": "\u00b1 0.010098",
+            "extra": "rounds: 5, min: 0.231310, max: 0.256220"
+          },
+          {
+            "name": "test_mahler_yum_2024",
+            "unit": "s",
+            "value": 4.0224362912020295,
+            "range": "\u00b1 0.031470",
+            "extra": "rounds: 5, min: 3.980430, max: 4.068864"
+          },
+          {
+            "name": "test_simulate[1000]",
+            "unit": "s",
+            "value": 0.47190196640003707,
+            "range": "\u00b1 0.023228",
+            "extra": "rounds: 5, min: 0.454453, max: 0.511410"
+          },
+          {
+            "name": "test_simulate[10000]",
+            "unit": "s",
+            "value": 3.257966672600014,
+            "range": "\u00b1 0.210219",
+            "extra": "rounds: 5, min: 2.899043, max: 3.449180"
+          },
+          {
+            "name": "test_solve[50]",
+            "unit": "s",
+            "value": 0.023483304479213984,
+            "range": "\u00b1 0.002351",
+            "extra": "rounds: 48, min: 0.019677, max: 0.029366"
+          },
+          {
+            "name": "test_solve[200]",
+            "unit": "s",
+            "value": 0.02343835269200029,
+            "range": "\u00b1 0.001612",
+            "extra": "rounds: 52, min: 0.020510, max: 0.026458"
+          },
+          {
+            "name": "test_solve[500]",
+            "unit": "s",
+            "value": 0.023627240893138397,
+            "range": "\u00b1 0.002499",
+            "extra": "rounds: 37, min: 0.019331, max: 0.031301"
+          },
+          {
+            "name": "test_mortality",
+            "unit": "s",
+            "value": 0.19661711880035,
+            "range": "\u00b1 0.005736",
+            "extra": "rounds: 5, min: 0.190841, max: 0.206028"
+          }
+        ]
       }
     ]
   }
